97bbd938eb43db4a6c24e63efb69e3ade2085cdb
slivermaker <2358066404@qq.com>
2025-09-22 07:48:31 +0800

    20250922

---
+++ 29bi大屏/ods_fgpsjb.sql
@@ -0,0 +1,45 @@
+id int 序号
+rq varchar 日期
+tfpbm varchar 退废品编码
+tfpdw varchar 退废品单位
+zfkbm varchar 支罚款编码
+zfkdw varchar 支罚款单位
+lxbs varchar 类型标示
+fplx varchar 废品类型
+pz varchar 品种
+gg varchar 规格
+xs varchar 形式
+dz float 单重
+js int 件数
+lry varchar 录入员
+gxsj datetime 更新时间
+zjgh varchar 质检工号
+zjgxm varchar 质检工姓名
+czzdm varchar 操作者代码
+fkbm1 varchar 罚款编码1
+fkdw1 varchar 罚款单位1
+lch varchar 炉次号
+bc varchar 班次
+jh varchar 件号
+czfl varchar 材质分类
+bmcl varchar 表面处理
+scgy varchar 
+scx varchar 生产线
+spdj decimal 索赔单价
+spe decimal 索赔额
+Djgxbz varchar 单价更新备注
+bz varchar 班组
+zfdw varchar 支付单位
+kj int 口径
+Zlczfl varchar 质量材质分类
+Tfpgx varchar 退废品工序
+fpsse decimal 
+gdh varchar 工单号
+kbh varchar 看板号
+zjbid int 
+sjly varchar 
+cpbm varchar 
+
+
+
+tablename=ods_fgpsjb
